b1f9811223bf887184bf762cd6eae3c4f4342c5b
msanchez563 <michaelsanchez563@u.boisestate.edu>
2019-04-12 12:43:40 -0600

    Removed unneccesary page/change about page

--- aboutfaq.php
+++ aboutfaq.php
@@ -22,18 +22,25 @@ session_start();
           </div>
           <div class = "dropdown">
             <form action = "index.php"><input type = "submit" value = "Home" /></form>
-            <form class = "search" action = "searchresult.php">
-            Click To Leave Movie Review:
-            <input id = "review" type = "submit" value = "Leave Review" /></form>
+            <form class="reviews" action="recentreviews.php">
+              Click To See Past Reviews:
+              <input id="moviereviews" type="submit" value="See Reviews" />
+            </form>
+            <?php
+              if (isset($_SESSION['status']) && $_SESSION['status'] == 'logged') {
+                echo "<form class = 'leavereview' action = 'moviereview.php'>";
+                echo "Click To Leave Movie Review: ";
+                echo "<input id = 'review' type = 'submit' value = 'Leave Review' />";
+                echo "</form>";
+              }
+            ?>
           </div>
         <div class = "aboutme">
-          <h1>AboutMe</h1>
-          <p>
-            My name is Michael Sanchez and this is my about me/FAQ page. Theres
-            not alot going on here but when I get it done you better believe
-            that this is going to be good well thats pretty much all I have so
-            have a good one.
-          </p>
+          <h1>AboutUs</h1>
+          <span id = "about">
+            My name is Michael Sanchez and this is my website for CS401-Web Development.
+            This website is rough but I tried hard on it so be nice. 
+          </span>
         </div>
         <div class = "footer">
           &copy; <a href = "aboutfaq.php">About Us</a> | <a href = "aboutfaq.php">FAQ</a>
